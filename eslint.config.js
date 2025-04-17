900bd305889f78b4ddb2a52f14eddf4fb947390c
vnabet <vnabet@groupeisagri.com>
2025-04-17 16:53:58 +0200

    mep de la config prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,43 +1,42 @@
 // @ts-check
-const eslint = require("@eslint/js");
-const tseslint = require("typescript-eslint");
-const angular = require("angular-eslint");
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+const eslintConfigPrettier = require('eslint-config-prettier/flat');
 
 module.exports = tseslint.config(
   {
-    files: ["**/*.ts"],
+    files: ['**/*.ts'],
     extends: [
       eslint.configs.recommended,
       ...tseslint.configs.recommended,
       ...tseslint.configs.stylistic,
       ...angular.configs.tsRecommended,
+      eslintConfigPrettier,
     ],
     processor: angular.processInlineTemplates,
     rules: {
-      "@angular-eslint/directive-selector": [
-        "error",
+      '@angular-eslint/directive-selector': [
+        'error',
         {
-          type: "attribute",
-          prefix: "lib",
-          style: "camelCase",
+          type: 'attribute',
+          prefix: 'lib',
+          style: 'camelCase',
         },
       ],
-      "@angular-eslint/component-selector": [
-        "error",
+      '@angular-eslint/component-selector': [
+        'error',
         {
-          type: "element",
-          prefix: "lib",
-          style: "kebab-case",
+          type: 'element',
+          prefix: 'lib',
+          style: 'kebab-case',
         },
       ],
     },
   },
   {
-    files: ["**/*.html"],
-    extends: [
-      ...angular.configs.templateRecommended,
-      ...angular.configs.templateAccessibility,
-    ],
+    files: ['**/*.html'],
+    extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
     rules: {},
-  }
+  },
 );
--- projects/ng-alize/src/lib/ng-alize.component.ts
+++ projects/ng-alize/src/lib/ng-alize.component.ts
@@ -3,13 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'alz-ng-alize',
   imports: [],
-  template: `
-    <p>
-      ng-alize works!
-    </p>
-  `,
-  styles: ``
+  template: ` <p>ng-alize works!</p> `,
+  styles: ``,
 })
 export class NgAlizeComponent {
-
+  constructor() {
+    console.log('ng-alize works!');
+  }
 }
