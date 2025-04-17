8dda3196a564d613adcede699e6c144ebaba8a91
vnabet <vnabet@groupeisagri.com>
2025-04-17 17:18:20 +0200

    installation de tailwind

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'demo';
-}
+export class AppComponent {}
