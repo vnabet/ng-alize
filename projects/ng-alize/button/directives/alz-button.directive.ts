1d1c5c71da99b3b78df73a257b9e490a9c4945aa
vnabet <vnabet@groupeisagri.com>
2025-04-23 17:42:59 +0200

    test de composition de directives

--- projects/ng-alize/button/directives/alz-button.directive.ts
+++ projects/ng-alize/button/directives/alz-button.directive.ts
@@ -1,23 +1,23 @@
-import { AfterViewInit, Directive, ElementRef, inject, input } from '@angular/core';
+import { Directive, ElementRef, inject } from '@angular/core';
+import { AlzTestDirective } from 'ng-alize/button/directives/alz-test.directive';
 
 @Directive({
   selector: '[alzButton]',
+  hostDirectives: [
+    {
+      directive: AlzTestDirective,
+      inputs: ['alzTest'],
+    },
+  ],
 })
-export class AlzButtonDirective implements AfterViewInit {
+export class AlzButtonDirective {
   readonly #el = inject(ElementRef);
-
-  readonly alzButton = input('toto');
 
   constructor() {
     const tagName = this.#el.nativeElement.tagName.toLowerCase();
     if (tagName !== 'button' && tagName !== 'a') throw new Error('alzButton can only be used on button or a elements');
 
     this.#baseStyle();
-  }
-
-  ngAfterViewInit() {
-    console.log('ngAfterViewInit', this.alzButton());
-    this.#el.nativeElement.classList.add('bg-orange-500');
   }
 
   #baseStyle() {
---
+++ projects/ng-alize/button/directives/alz-test.directive.spec.ts
@@ -0,0 +1,8 @@
+import { AlzTestDirective } from './alz-test.directive';
+
+describe('AlzTestDirective', () => {
+  it('should create an instance', () => {
+    const directive = new AlzTestDirective();
+    expect(directive).toBeTruthy();
+  });
+});
