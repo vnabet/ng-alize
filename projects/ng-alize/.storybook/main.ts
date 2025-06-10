2328bcd38aa4238c7d6a2b0da992cedd6289d0c9
vnabet <vnabet@groupeisagri.com>
2025-06-10 10:46:26 +0200

    maj storybook

--- projects/ng-alize/.storybook/main.ts
+++ projects/ng-alize/.storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
   stories: ['../**/stories/**/*.mdx', '../**/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-onboarding', '@storybook/addon-interactions'],
+  addons: ['@storybook/addon-onboarding', '@storybook/addon-docs'],
   framework: {
     name: '@storybook/angular',
     options: {},
